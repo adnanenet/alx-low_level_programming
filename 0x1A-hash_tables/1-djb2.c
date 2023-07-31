c7bacbfb455b838c9f5ea8ea0bfcacbfca973b33
ADNANE BEN SOUDA <76470837+adnanebensouda@users.noreply.github.com>
2023-07-31 21:10:20 +0100

    Update 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -2,19 +2,19 @@
 
 /**
  * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generat hash value
+ * @str: string used to generate hash value
  *
  * Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-	int v;
+	int c;
 
 	hash = 5381;
-	while ((v = *str++))
+	while ((c = *str++))
 	{
-		hash = ((hash << 5) + hash) + v; /* hash * 33 + v */
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
 	return (hash);
 }
